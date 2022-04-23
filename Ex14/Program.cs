af8ade92972e5582c6ca90a06a8a9f6e3b61c867
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 15:00:25 +0300

    Variant

---
+++ Ex14/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координату X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x == 0 && y == 0) Console.WriteLine("Точка находится в начале координат");
+if(x == 0 && y < 0) Console.WriteLine("Точка находится на границе  III и IV четверти");
+if(x == 0 && y > 0) Console.WriteLine("Точка находится на границе  I и II четверти");
+if(x > 0 && y == 0) Console.WriteLine("Точка находится на границе  I и IV четверти");
+if(x < 0 && y == 0) Console.WriteLine("Точка находится на границе  II и III четверти");
+if(x < 0 && y < 0) Console.WriteLine("Точка находится в III четверти");
+if(x > 0 && y > 0) Console.WriteLine("Точка находится в I четверти");
+if(x < 0 && y > 0) Console.WriteLine("Точка находится во II четверти");
+if(x > 0 && y < 0) Console.WriteLine("Точка находится в IV четверти");
+
