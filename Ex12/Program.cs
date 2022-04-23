7a45236fd060f2a09298e8fa23e70673ad6742f4
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 13:37:12 +0300

    add new folder

---
+++ Ex12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите отрицательное четырехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(-9999 < a && a < -1000)
+{
+    int b = a / (-10);
+    int c = b / 10;
+    int d = c * 10;
+    int f = b - d;
+    System.Console.WriteLine($"Вторая цифра числа: {f}");
+}
+else
+{
+    Console.WriteLine("Введенное число не соответвует условиям");
+}
