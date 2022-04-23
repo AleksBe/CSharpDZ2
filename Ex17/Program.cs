bbb03a999fb530962f68566747a647a1c3b94194
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 23:53:05 +0300

    Variant

---
+++ Ex17/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+while(a <= n)
+{
+    System.Console.WriteLine($"{Math.Pow(a, 3)}");
+    a++;
+}
