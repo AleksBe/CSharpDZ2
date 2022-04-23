8759b80612b28bcde22347be3a87a7d09b881d46
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 13:35:17 +0300

    add new folder

---
+++ Ex9/Program.cs
@@ -0,0 +1,11 @@
+﻿int SecondFigure()// Сделаем в виде метода
+{
+int value = new Random().Next(100, 1000); // Пусть число будет случайным
+System.Console.WriteLine($"Заданное число:{value}"); // Будем выводить число, для наглядности
+
+int x = value / 10;
+int a = x % 10;
+return a;
+}
+
+System.Console.WriteLine($"Вторая цифра заданного числа:{SecondFigure()}");
