0aa8df94d41a18501be05403d965de1633ad950d
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 13:34:36 +0300

    add new folder

---
+++ Ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetNumber()
+{
+int value = new Random().Next(10, 100);
+
+int a = value / 10;
+int b = value % 10;
+
+System.Console.WriteLine($"value: {value}");
+int max = a;
+if (b > max) max = b;
+return max;
+}
+System.Console.WriteLine($"Максимальное число: {GetNumber()}");
