ab50d1e13427e7188f4237aa83df3f9b1668192e
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 13:33:18 +0300

    add new folders

---
+++ Ex7/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = new Random().Next(10, 100); // Первое число - случайное двузначное число
+int b = new Random().Next(1, 10); // Второе число - случайное однозначное число
+System.Console.WriteLine($"a: {a} b: {b}"); // Чтобы понимать с какими числами мы работаем
+
+int c = a % b;
+
+
+if(c > 0) System.Console.WriteLine($"Число b не кратно числу а, остаток от деления {c}");
+else Console.WriteLine("Число b кратно числу a");
