99534f5cc4e0a87322ddf303c19de15ec3d9593b
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-23 13:37:50 +0300

    add new folder

---
+++ Ex13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число от 1 до 7, обозначающее день недели и мы скажем Вам является ли этот день выходным");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a <= 0 ^ a > 7)
+{
+    Console.WriteLine ("Введено некорректное число");
+}
+
+else
+{ 
+    if(a > 5 && a < 8)
+    {
+    Console.WriteLine("Да");
+    }
+    else
+    {
+    Console.WriteLine("Нет");
+    }
+}
