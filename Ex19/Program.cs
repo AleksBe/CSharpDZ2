ee2436d5d3ef322a991ca372a834959da2c962fc
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-29 17:10:51 +0300

    Variant2

--- Ex19/Program.cs
+++ Ex19/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
 // При написании программы 0 считать натуральным числом не будем.
 
-Console.WriteLine("Введите число А:");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B:");
-int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число А:");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число B:");
+//int b = Convert.ToInt32(Console.ReadLine());
 
 
-double Stepen()
+double Stepen(int a, int b)
 {
     double result = a;
     int i = 2;
@@ -17,5 +17,5 @@ double Stepen()
     }
     return result;
 }
-double final = Stepen();
+double final = Stepen(a: 56, b: 7);  // Вариант, когда числа А и В не вводятся пользователем а "зашиваются" в метод.
 Console.WriteLine(final);
