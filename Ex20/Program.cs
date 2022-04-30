b585d32432852354ced417ff2b7c305593a1edd9
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-04-30 16:01:49 +0300

    Variant

--- Ex20/Program.cs
+++ Ex20/Program.cs
@@ -1,67 +1,18 @@
 ﻿// Написать программу, которая принимает на вход число и выдает сумму цифр в числе.
 
+// Данная программа будет работать только для целых чисел (положительных и отрицательных)
+
 Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
+int a = Convert.ToInt32(Console.ReadLine());
+int number = Math.Abs(a);      // Если введут отрицательное число
 
-int len = number.Length;
+int summ = number % 10;
 
-Console.WriteLine(len);
-int summ = 0;
-for(int i = 0; i < len; i++)
-   {
-      summ = summ + number[i];
-   }
-   Console.WriteLine(summ);
-//int[] array = new int [len];
+while(number / 10 > 0)
+{
+   number = number / 10;
+   summ = summ + number % 10;
+}
 
-//int summ = 0;
-//for(int i = 0; i < len; i++)
-//{
-   //array[i] = Convert.ToInt32(number[i]);
-  // Console.WriteLine(array[i]);
-   //summ = summ + array[i];
-    
-//}
-//Console.WriteLine(summ);
-
-//int[] CreateArray(int len)  // Создаем массив
-//{
-    //int[] result = new int[len];
-    //return result;
-//}
-
-//int FillArrayAndSumm(int[] array) // Заполняем массив
-//{
-   // int Summ = 0;
-   // for(int i = 0; i < len; i++)
-   // {
-      //  array[i] = number[i];
-      //  Summ = Summ + array[i];
-    //}
-   // return Summ;
-//}
-//int Summa(int[] array) // Считаем сумму элементов массива
-//{
- //   int Summ = 0;
- //   for(int i = 0; i < len; i++);
-  //  {
-    //    Summ = Summ + array[i];
-   // }
-   // return Summ;
-//}
-
-//int[] arr = CreateArray(len);
-//Summa(FillArray(arr));
-//Console.WriteLine($"Сумма цифр введенного числа = {FillArrayAndSumm(arr)}");
-
-
-//int Summ = 0;
-//int i = 0;
-//int len = array.Lenth;
-
-//for(i = 0; i < len; i++)
-//{
-    //Summ = Summ + array[i];
-//}
-//Console.WriteLine($"Сумма цифр числа = {Summ} ");
+Console.WriteLine($"Сумма цифр числа = {summ}");
 
